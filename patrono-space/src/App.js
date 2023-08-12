94ceef430a87fb83e8fff2d799fd04081516c8c7
gab.coutinho <eng.gabrielcoutinho@outlook.com.br>
2023-08-12 20:58:02 -0300

    Development of banner into App.js

--- patrono-space/src/App.js
+++ patrono-space/src/App.js
@@ -1,14 +1,31 @@
-import sty from './styles/styGlobal.scss';
+
+import styApp from './styles/App.module.scss';
+
 import Header from './components/Header';
 import Menu from './components/Menu';
 
+import banner from './styles/assets/banner.png';
+
+
 function App() {
   return (
-    <div className={sty.App}>
+    <>
       <Header />
+      <main>
+        <section className={styApp.menu_banner}>
+          <Menu />
+          <div className={styApp.menu_banner__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img
+              
+              src={banner}
+              alt='banner'
+            />
+          </div>
+        </section>
+      </main>
 
-      <Menu />
-    </div>
+    </>
   );
 }
 
