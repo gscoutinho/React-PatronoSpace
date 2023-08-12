a61592b4cde0085ef86c1d54dec2b4f85ce485fa
gab.coutinho <eng.gabrielcoutinho@outlook.com.br>
2023-08-12 17:23:14 -0300

    Desenvolvimento do menu

--- patrono-space/src/components/Menu/index.jsx
+++ patrono-space/src/components/Menu/index.jsx
@@ -1,8 +1,53 @@
 
 import sty from './Menu.module.scss'
 
+import iconHomeAct from './assets/home-ativo.png'
+import iconLiked from './assets/mais-curtidas-inativo.png'
+import iconSeen from './assets/mais-vistas-inativo.png'
+import iconNew from './assets/novas-inativo.png'
+import iconSurprise from './assets/surpreenda-me-inativo.png'
+
+
 export default function Menu() {
   return (
-    <div>Menu</div>
+    <nav className={sty.menu}>
+        <ul className={sty.menu__lista}>
+            <li className={sty.menu__item}>
+                <img
+                src={iconHomeAct}
+                alt='Home'
+                />
+                <a href="/">Início</a>
+            </li>
+            <li className={sty.menu__item}>
+                <img
+                src={iconSeen}
+                alt='Home'
+                />
+                <a href="/">Mais vistas</a>
+            </li>
+            <li className={sty.menu__item}>
+                <img
+                src={iconLiked}
+                alt='Home'
+                />
+                <a href="/">Mais curtidas</a>
+            </li>
+            <li className={sty.menu__item}>
+                <img
+                src={iconNew}
+                alt='Home'
+                />
+                <a href="/">Novas</a>
+            </li>
+            <li className={sty.menu__item}>
+                <img
+                src={iconSurprise}
+                alt='Home'
+                />
+                <a href="/">Surpreenda-me</a>
+            </li>
+        </ul>
+    </nav>
   )
 }
